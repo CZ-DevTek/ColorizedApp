7b76eb653efb57ad964b53d179ab115cdbbb4a61
Carlos García Pérez <carlosziok@gmail.com>
2024-04-20 15:39:20 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Carlos Garcia Perez on 20/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorized/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Colorized
+//
+//  Created by Carlos Garcia Perez on 20/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
